f145b7ab53a4919d4a577ac6d80ff053fa88a994
Sam Soffes <sam@samsoff.es>
2010-08-18 22:58:16 -0500

    Cleaned up views

--- Classes/MDSidebarBorderView.m
+++ Classes/MDSidebarBorderView.m
@@ -34,100 +34,66 @@
 #import "Foundation/NSGeometry.h"
 #import "MDSettings.h"
 
-#pragma mark -
-#pragma mark Original methods
+NSComparisonResult compareFrameOriginX(id viewA, id viewB, void *context) {
+    float v1 = [viewA frame].origin.x;
+    float v2 = [viewB frame].origin.x;
+    
+	if (v1 < v2) {
+        return NSOrderedAscending;
+	} else if (v1 > v2) {
+        return NSOrderedDescending;
+	}
+	
+	return NSOrderedSame;
+}
 
 @implementation MDSidebarBorderView
 
-- (id) initWithFrame:(NSRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-    }
-    return self;
+#pragma mark Class Methods
+
++ (NSImage *)bundledImageWithName:(NSString *)imageName {
+	NSBundle *pluginBundle = [NSBundle bundleForClass:[self class]];
+	return [[[NSImage alloc] initWithContentsOfFile:[pluginBundle pathForResource:imageName ofType:@"png"]] autorelease];
 }
 
-#pragma mark -
-#pragma mark Overridden from NSView
 
-//for convenience
-- (BOOL) mouseDownCanMoveWindow {
+#pragma mark NSView
+
+- (BOOL)mouseDownCanMoveWindow {
     return YES;
 }
 
-- (void) drawRect:(NSRect)rect {
-    // Drawing code here.
-    NSRect fromRect;
-    NSImage* image = [MDSidebarBorderView bundledImageWithName:@"DrawerBorder"];
-    fromRect.size=[image size];
-    fromRect.origin.x=0;
-    fromRect.origin.y=0;
+
+- (void)drawRect:(NSRect)rect {
+    NSRect fromRect = NSZeroRect;
+    NSImage *image = [MDSidebarBorderView bundledImageWithName:@"DrawerBorder"];
+    fromRect.size = [image size];
 	
     [image drawInRect:[self frame] fromRect:fromRect operation:NSCompositeSourceOver fraction:1.0];
 }
 
-#pragma mark -
-#pragma mark Sidebar conversion
 
-int compareFrameOriginX(id viewA, id viewB, void *context) {
-    float v1 = [viewA frame].origin.x;
-    float v2 = [viewB frame].origin.x;
-    if (v1 < v2)
-        return NSOrderedAscending;
-    else if (v1 > v2)
-        return NSOrderedDescending;
-    else
-        return NSOrderedSame;
-}
+#pragma mark Drawing
 
-- (void) terminalButtonPressed:(id)sender {
-	
-    NSArray* selectedItems;
-    if(projectFileOutlineView && 
-	   [projectFileOutlineView respondsToSelector:@selector(selectedItems)]) {
-        selectedItems = [projectFileOutlineView performSelector:@selector(selectedItems)];
-        if(!selectedItems || ![selectedItems isKindOfClass:[NSArray class]] || [selectedItems count]==0) {
-            return;
-        }
-    }
-	
-    for (NSDictionary* item in selectedItems) {
-        MDLog("[projectFileOutlineView selectedItems]: %@", item);
-        NSString* path = [item objectForKey:@"sourceDirectory"];
-        if (!path) {
-            path = [[item objectForKey:@"filename"] stringByDeletingLastPathComponent];
-        }
-        if (path) {
-            path = [path stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\\\\\""];
-            NSString *s = [NSString stringWithFormat:@"tell application \"Terminal\"\n\tdo script \"cd \\\"%@\\\"\"\n\tactivate\nend tell", path];
-            MDLog("script:\n%@", s);
-            NSAppleScript *as = [[NSAppleScript alloc] initWithSource: s];
-            [as executeAndReturnError:nil];
-			[as release];
-            return;
-        }
-    }
-}
-
-- (void) addToSuperview:(NSView*)superview {
+- (void)addToSuperview:(NSView*)superview {
     NSScrollView* outlineView = nil;
     int i, cnt;
 	BOOL showSidebarOnLeft = [[MDSettings defaultSettings] showSideViewOnLeft];
 	
-    //adjust frame
-    NSImage* image = [MDSidebarBorderView bundledImageWithName:@"DrawerBorder"];
-    NSRect borderRect;
-	borderRect.origin.x = showSidebarOnLeft?-1.0:1.0;
-    borderRect.origin.y = 0.0;
+    // Adjust frame
+    NSImage *image = [MDSidebarBorderView bundledImageWithName:@"DrawerBorder"];
+    NSRect borderRect = NSZeroRect;
+	borderRect.origin.x = showSidebarOnLeft ? -1.0 : 1.0;
     borderRect.size.height = [image size].height;
-    borderRect.size.width = [superview frame].size.width+2;
+    borderRect.size.width = [superview frame].size.width + 2;
 	
-    //add resizer image
+    // Add resizer image
     NSRect handleRect = NSZeroRect;
-    NSImage* sidebarResizerImage = [MDSidebarBorderView bundledImageWithName:@"DrawerResizeHandle"];
+    NSImage *sidebarResizerImage = [MDSidebarBorderView bundledImageWithName:@"DrawerResizeHandle"];
     handleRect.size = [sidebarResizerImage size];
     handleRect.origin.y = 0;
 	if (showSidebarOnLeft) {
-		handleRect.origin.x = [self frame].size.width-handleRect.size.width;
+		handleRect.origin.x = [self frame].size.width - handleRect.size.width;
 	} else {
 		handleRect.origin.x = 0;
 	}
@@ -139,26 +105,26 @@ int compareFrameOriginX(id viewA, id viewB, void *context) {
 	
     [self setAutoresizingMask:(NSViewWidthSizable+NSViewMaxYMargin)];
 	
-    //add self to superview
+    // Add self to superview
     [superview addSubview:self];
     [self setFrame:borderRect];
 	
-    //gather buttons
-    NSMutableArray* btns=[[NSMutableArray alloc]init];
-    NSArray* subviews=[superview subviews];
-    cnt=[subviews count];
-    for (i=0; i<cnt; i++) {
-        id aView=[subviews objectAtIndex:i];
-        if([aView isKindOfClass:[NSButton class]] && [aView frame].origin.y<1){
+    // Gather buttons
+    NSMutableArray *btns = [[NSMutableArray alloc] init];
+    NSArray *subviews = [superview subviews];
+    cnt = [subviews count];
+    for (i = 0; i < cnt; i++) {
+        id aView = [subviews objectAtIndex:i];
+        if ([aView isKindOfClass:[NSButton class]] && [aView frame].origin.y < 1) {
             [btns addObject:aView];
-        }else if([aView isKindOfClass:[NSScrollView class]]){
-            outlineView=(NSScrollView*)aView;
+        } else if ([aView isKindOfClass:[NSScrollView class]]) {
+            outlineView = (NSScrollView *)aView;
             projectFileOutlineView = [[outlineView contentView] documentView];
         }
     }
 	
     [btns sortUsingFunction:(NSInteger (*)(id, id, void *))compareFrameOriginX context:nil];
-    NSButton* lastButton = [btns lastObject];
+    NSButton *lastButton = [btns lastObject];
 	
     NSRect terminalButtonFrame;
     terminalButtonFrame.size.width = 23;
@@ -166,10 +132,10 @@ int compareFrameOriginX(id viewA, id viewB, void *context) {
     terminalButtonFrame.origin.x = [lastButton frame].origin.x + terminalButtonFrame.size.width;
     terminalButtonFrame.origin.y = [lastButton frame].origin.y;
 	
-    NSButton* terminalButton = [[NSButton alloc] initWithFrame:terminalButtonFrame];
+    NSButton *terminalButton = [[NSButton alloc] initWithFrame:terminalButtonFrame];
 	
-    NSImage* buttonImage = [MDSidebarBorderView bundledImageWithName:@"OpenTerminalHere"];
-    NSImage* buttonImagePressed = [MDSidebarBorderView bundledImageWithName:@"OpenTerminalHerePressed"]; 
+    NSImage *buttonImage = [MDSidebarBorderView bundledImageWithName:@"OpenTerminalHere"];
+    NSImage *buttonImagePressed = [MDSidebarBorderView bundledImageWithName:@"OpenTerminalHerePressed"]; 
 	
 	[terminalButton setToolTip:@"Open Terminal window and 'cd' to selected file/folder"];
     [terminalButton setImage:buttonImage];
@@ -181,25 +147,24 @@ int compareFrameOriginX(id viewA, id viewB, void *context) {
     [btns addObject:terminalButton];
 	[terminalButton release];
 	
-    //[btns sortUsingFunction:(NSInteger (*)(id, id, void *))compareFrameOriginX context:nil];
+//  [btns sortUsingFunction:(NSInteger (*)(id, id, void *))compareFrameOriginX context:nil];
 	
-	//adjust outlineView frame
-    if(outlineView){
-        NSRect aRect=[superview frame];
-        aRect.origin.x=-1.0;
-        aRect.origin.y=[self frame].size.height;
-        aRect.size.height=aRect.size.height-aRect.origin.y+1;
-        aRect.size.width=aRect.size.width+2;
+	// Adjust outlineView frame
+    if (outlineView){
+        NSRect aRect = [superview frame];
+        aRect.origin.x = -1.0;
+        aRect.origin.y = [self frame].size.height;
+        aRect.size.height = aRect.size.height-aRect.origin.y+1;
+        aRect.size.width = aRect.size.width+2;
         [outlineView setFrame:aRect];
 		
-        NSOutlineView* realOutlineView = [outlineView documentView];
+        NSOutlineView *realOutlineView = [outlineView documentView];
         NSFont *font = [NSFont fontWithName:@"Lucida Grande" size:12];
-        NSLayoutManager* layoutManager = [NSLayoutManager new]; 
+        NSLayoutManager *layoutManager = [NSLayoutManager new]; 
         [realOutlineView setRowHeight:[layoutManager defaultLineHeightForFont:font]];
         [layoutManager release];
         [realOutlineView setIntercellSpacing:NSMakeSize (6.0, 4.0)];
         [realOutlineView reloadData];
-		
     }
 	
 	//place buttons into sideboardview (self)
@@ -240,12 +205,11 @@ int compareFrameOriginX(id viewA, id viewB, void *context) {
 //	}
 	
 	float leftLoc = 0;
-	if(!showSidebarOnLeft) {
+	if (!showSidebarOnLeft) {
 		leftLoc = leftLoc + handleRect.size.width; // = 18 
-							 
 	}
-	for (NSView* button in btns) {
-		
+	
+	for (NSView *button in btns) {
 		NSRect buttonFrame = [button frame];
 		buttonFrame.origin.y = -4;
 		buttonFrame.origin.x = leftLoc;
@@ -255,21 +219,42 @@ int compareFrameOriginX(id viewA, id viewB, void *context) {
 		[button removeFromSuperview];
 		[button setFrame:buttonFrame];
 		[self addSubview:button];
-		
 	}
-	
-	
 	
     [btns release];
     [imageView release];
 }
 
-#pragma mark -
-#pragma mark Images
 
-+ (NSImage*) bundledImageWithName:(NSString*)imageName {
-	NSBundle* pluginBundle = [NSBundle bundleForClass:[self class]];
-	return [[[NSImage alloc] initWithContentsOfFile:[pluginBundle pathForResource:imageName ofType:@"png"]] autorelease];
+#pragma mark Actions
+
+- (void)terminalButtonPressed:(id)sender {
+    NSArray *selectedItems = nil;
+    if (projectFileOutlineView && 
+		[projectFileOutlineView respondsToSelector:@selector(selectedItems)]) {
+        selectedItems = [projectFileOutlineView performSelector:@selector(selectedItems)];
+        if (!selectedItems || ![selectedItems isKindOfClass:[NSArray class]] || [selectedItems count] == 0) {
+            return;
+        }
+    }
+	
+    for (NSDictionary *item in selectedItems) {
+        MDLog("[projectFileOutlineView selectedItems]: %@", item);
+        NSString *path = [item objectForKey:@"sourceDirectory"];
+        if (!path) {
+            path = [[item objectForKey:@"filename"] stringByDeletingLastPathComponent];
+        }
+		
+        if (path) {
+            path = [path stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\\\\\""];
+            NSString *s = [NSString stringWithFormat:@"tell application \"Terminal\"\n\tdo script \"cd \\\"%@\\\"\"\n\tactivate\nend tell", path];
+            MDLog("script:\n%@", s);
+            NSAppleScript *as = [[NSAppleScript alloc] initWithSource: s];
+            [as executeAndReturnError:nil];
+			[as release];
+            return;
+        }
+    }
 }
 
 @end
