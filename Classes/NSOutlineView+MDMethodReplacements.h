43c97e36bedd51032689ca450e85cf98e9cebed9
Jon Lee <jonlee554@gmail.com>
2011-08-07 01:11:19 -0700

    basic functionality for filtering

---
+++ Classes/NSOutlineView+MDMethodReplacements.h
@@ -0,0 +1,13 @@
+//
+//  NSOutlineView+MDMethodReplacements.h
+//  MissingDrawer
+//
+//  Created by Jon Lee on 8/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSOutlineView_MDMethodReplacements : NSObject
+
+@end
---
+++ Classes/NSOutlineView+MDMethodReplacements.m
@@ -0,0 +1,13 @@
+//
+//  NSOutlineView+MDMethodReplacements.m
+//  MissingDrawer
+//
+//  Created by Jon Lee on 8/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSOutlineView+MDMethodReplacements.h"
+
+@implementation NSOutlineView_MDMethodReplacements
+
+@end
